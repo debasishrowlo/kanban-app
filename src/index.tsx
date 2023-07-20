de88899744ec1555c386d66038ab56eea5cc2099
debasishrowlo <debasishrowlo@gmail.com>
2023-07-21 01:26:58 +0530

    Add column styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,159 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
 
-import logoMobileImg from "./assets/images/logo-mobile.svg"
-import chevronDownImg from "./assets/images/icon-chevron-down.svg"
-import addTaskMobileImg from "./assets/images/icon-add-task-mobile.svg"
-import verticalEllipsisImg from "./assets/images/icon-vertical-ellipsis.svg"
+import Board from "./Board"
 
 import "./index.css"
 
+export type Board = {
+  name: string,
+  columns: Array<{
+    name: string,
+    tasks: Array<{
+      name: string,
+      description: string,
+      subtasks: Array<{
+        title: string,
+        complete: boolean,
+      }>
+    }>,
+  }>,
+}
+
 const App = () => {
-  return (
-    <div className="h-screen flex flex-col">
-      <div className="p-4 flex items-center justify-between bg-white">
-        <div className="flex items-center">
-          <img src={logoMobileImg} />
-          <p className="ml-4 text-18 font-bold">Platform Launch</p>
-          <img src={chevronDownImg} className="w-2 ml-2" />
-        </div>
-        <div className="flex items-center">
-          <button type="button" className="px-4 py-2 bg-purple opacity-25 rounded-full shadow-md">
-            <img src={addTaskMobileImg} className="w-3" />
-          </button>
-          <button type="button" className="px-4">
-            <img src={verticalEllipsisImg} />
-          </button>
-        </div>
-      </div>
-      <div className="grow flex items-center">
-        <div>
-          <p className="px-8 text-center text-18 font-bold text-gray-300">
-            This board is empty. Create a new column to get started.
-          </p>
-          <button type="button" className="px-5 py-3.5 mt-6 mx-auto block bg-purple text-14 font-bold text-white rounded-full">
-            <div className="flex items-center">
-              <img src={addTaskMobileImg} className="w-2.5" />
-              <span className="ml-2">Add New Column</span>
-            </div>
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+  const boards:Board[] = [
+    {
+      name: "Platform Launch",
+      columns: [
+        {
+          name: "Todo",
+          tasks: [
+            {
+              name: "Research pricing points of various competitors and trial different business models",
+              description: "We know what we're planning to build for version one. Now we need to finalise the first pricing model we'll use. Keep iterating the subtasks until we have a coherent proposition.",
+              subtasks: [
+                {
+                  title: "Research competitor pricing and business models",
+                  complete: true,
+                },
+                { 
+                  title: "Research competitor pricing and business models",
+                  complete: false,
+                },
+              ],
+            },
+            {
+              name: "Build UI for onboarding flow",
+              description: "",
+              subtasks: [
+                {
+                  title: "Research competitor pricing and business models",
+                  complete: true,
+                },
+              ],
+            },
+            {
+              name: "Build UI for search",
+              description: "",
+              subtasks: [
+                {
+                  title: "Research competitor pricing and business models",
+                  complete: true,
+                },
+              ],
+            },
+            {
+              name: "Build settings UI",
+              description: "",
+              subtasks: [
+                {
+                  title: "Research competitor pricing and business models",
+                  complete: true,
+                },
+              ],
+            },
+          ]
+        },
+        {
+          name: "Doing",
+          tasks: [
+            {
+              name: "Research pricing points of various competitors and trial different business models",
+              description: "We know what we're planning to build for version one. Now we need to finalise the first pricing model we'll use. Keep iterating the subtasks until we have a coherent proposition.",
+              subtasks: [
+                {
+                  title: "Research competitor pricing and business models",
+                  complete: true,
+                },
+              ],
+            },
+            {
+              name: "Design settings and search pages",
+              description: "",
+              subtasks: [
+                {
+                  title: "Research competitor pricing and business models",
+                  complete: true,
+                },
+              ],
+            },
+            {
+              name: "Add account management endpoints",
+              description: "",
+              subtasks: [
+                {
+                  title: "Research competitor pricing and business models",
+                  complete: true,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: "Done",
+          tasks: [
+            {
+              name: "Research pricing points of various competitors and trial different business models",
+              description: "We know what we're planning to build for version one. Now we need to finalise the first pricing model we'll use. Keep iterating the subtasks until we have a coherent proposition.",
+              subtasks: [
+                {
+                  title: "Research competitor pricing and business models",
+                  complete: true,
+                },
+              ],
+            },
+            {
+              name: "Design settings and search pages",
+              description: "",
+              subtasks: [
+                {
+                  title: "Research competitor pricing and business models",
+                  complete: true,
+                },
+              ],
+            },
+            {
+              name: "Add account management endpoints",
+              description: "",
+              subtasks: [
+                {
+                  title: "Research competitor pricing and business models",
+                  complete: true,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Marketing Plan",
+      columns: [],
+    },
+  ]
+  const activeBoardIndex = 0
+
+  return <Board board={boards[activeBoardIndex]} />
 }
 
 const container = document.getElementById("app")
